f2e9e291736d93123bebec10ecdf3fe527d14e25
Pujari Varun <155567840+pujari-varun@users.noreply.github.com>
2024-03-29 00:05:35 +0530

    binary method is used

--- EvenOdd.java
+++ EvenOdd.java
@@ -3,7 +3,7 @@ public class EvenOdd{ 	public static void main(String[] ars){
 		int x;
 		Scanner input=new Scanner(System.in);
 		x=input.nextInt();
-		if(x%2==0){
+		if((x&1)==0){
 			System.out.println("even");
 }
 else{ System.out.println("odd");

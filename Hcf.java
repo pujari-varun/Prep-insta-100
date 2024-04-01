047805c3159144b61721983dd266725a98a57367
Pujari Varun <155567840+pujari-varun@users.noreply.github.com>
2024-04-01 20:01:23 +0530

    highest common factor

---
+++ Hcf.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Hcf{
+public static void main(String[] args){
+int x,y;
+Scanner input=new Scanner(System.in);
+System.out.println("enter the numbers");
+x=input.nextInt();
+y=input.nextInt();
+int h=1;
+for(int i=1;i<=x && i<=y;i++){
+if(x%i==0 && y%i==0){
+h=i;
+}
+}
+System.out.println("HCF:"+h);
+}
+}
